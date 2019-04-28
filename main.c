731bbcf96e05e44490852fee43af0f899a1d00bc
Angus Vong <vtf888@hotmail.com>
2019-04-28 12:58:57 +0000

    add some beginning

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
+
 #include <stdio.h>
 
-int main(){
-
-return 0;
+int main()
+{
+    FILE * input;
+    char fn[256];
+    scanf("%s", fn);
+    input = fopen(fn, "r");
+    
+    while(feof(input) == 0){
+        char c;
+        
+        fscanf(input, "%c", &c);
+        
+        doEncryption(&c);
+        
+        printf("%c\n", c);
+    }
 }
