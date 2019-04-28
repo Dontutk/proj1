0f2a63a00dc6ae01f7bb6583cc6e39bc29f12a89
Angus Vong <vtf888@hotmail.com>
2019-04-28 14:42:56 +0000

    finished Rotation Cipher's Encryption

--- main.c
+++ main.c
@@ -4,17 +4,31 @@
 int main()
 {
     FILE * input;
-    char fn[256];
-    scanf("%s", fn);
-    input = fopen(fn, "r");
+    int k;
+    input = fopen("input.txt", "r");
+    scanf("%d", &k);
+    
+    
+    printf("The encrypted text (Rotation Cipher) is : \n");
     
     while(feof(input) == 0){
         char c;
         
         fscanf(input, "%c", &c);
-        
-        doEncryption(&c);
-        
-        printf("%c\n", c);
+        if(c >= 97 && c<=122)
+        c = c + k;
+        if (c>122 )
+        c = c - 26;
+        if(c >= 65 && c<=90)
+        c = c + k;
+        if (c>90 && c <= 90 + k)
+        c = c - 26;
+        printf("%c", c);
     }
+
+        
+        
+        printf("The key is: %d ", k);
+    
+
 }
